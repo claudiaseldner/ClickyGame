91f411f00424b6c23ebc675f1501bd9621d812db
Claudia Seldner <claudiaseldner@Claudias-MacBook-Pro-2.local>
2019-07-26 16:33:59 -0500

    adding react functions and images

--- my-app/src/components/Navbar/Navbar.js
+++ my-app/src/components/Navbar/Navbar.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './Navbar.css';
 
 const Navbar = props => (
-  <div className="navbar ">
+  <div className="navbar">
     <div>Clicky Game</div>
-    <div className={props.navMsgColor}>{props.navMessage}</div>
+    <div className={props.Message2}>{props.Message}</div>
     <div>
       Score: {props.score} <span className="score">|</span> Top Score: {props.highScore}
     </div>
